8e95f4e419102d9ec7117362e093cbae20f79cc0
furxx2000 <furxx2000@gmail.com>
2023-03-21 01:22:56 +0800

    Add Hello world and form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { HelloWorld } from './component/HelloWorld';
+import { Form } from './component/Form';
+import { useForm } from './hook/useForm';
+import { apiCall } from './ApiCall';
+import './css/App.css';
+
+// interface LoginReq {
+//   account: string;
+//   pwd: string;
+// }
+
+const validationRule = {
+  account: (v: string) => v.length > 8,
+  pwd: (v: string) => v.length > 12,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loginReq, formSetter, validationResult } = useForm(
+    {
+      account: '',
+      pwd: '',
+    },
+    validationRule
+  );
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const isValid = validationResult.allFieldsValid();
+
+    if (isValid) {
+      console.log('Valid');
+    } else {
+      console.log('Submit fail!');
+    }
+
+    // if (validationResult.allFieldsValid()) {
+    //   console.log('All fields valid');
+    // } else {
+    //   console.log('Submit fail');
+    // }
+  }
+
+  function foo() {
+    return apiCall(3, 'Hello world!');
+  }
+
+  function bar() {
+    return apiCall(4, 2023);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main>
+      <HelloWorld foo={foo} bar={bar} />
+      <Form
+        loginReq={loginReq}
+        formSetter={formSetter}
+        validationResult={validationResult}
+        handleSubmit={handleSubmit}
+      />
+    </main>
+  );
 }
 
-export default App
+export default App;
