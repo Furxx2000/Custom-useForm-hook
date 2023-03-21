5a90aec21ebe68a64ad5552a6be219d6e4654ec4
furxx2000 <furxx2000@gmail.com>
2023-03-22 00:30:53 +0800

    Use useReducer to manage state

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,9 @@
 import { HelloWorld } from './component/HelloWorld';
 import { Form } from './component/Form';
-import { useForm } from './hook/useForm';
 import { apiCall } from './ApiCall';
 import './css/App.css';
 
-// interface LoginReq {
-//   account: string;
-//   pwd: string;
-// }
-
-const validationRule = {
-  account: (v: string) => v.length > 8,
-  pwd: (v: string) => v.length > 12,
-};
-
 function App() {
-  const { loginReq, formSetter, validationResult } = useForm(
-    {
-      account: '',
-      pwd: '',
-    },
-    validationRule
-  );
-
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    const isValid = validationResult.allFieldsValid();
-
-    if (isValid) {
-      console.log('Valid');
-    } else {
-      console.log('Submit fail!');
-    }
-
-    // if (validationResult.allFieldsValid()) {
-    //   console.log('All fields valid');
-    // } else {
-    //   console.log('Submit fail');
-    // }
-  }
-
   function foo() {
     return apiCall(3, 'Hello world!');
   }
@@ -52,12 +15,7 @@ function App() {
   return (
     <main>
       <HelloWorld foo={foo} bar={bar} />
-      <Form
-        loginReq={loginReq}
-        formSetter={formSetter}
-        validationResult={validationResult}
-        handleSubmit={handleSubmit}
-      />
+      <Form />
     </main>
   );
 }
