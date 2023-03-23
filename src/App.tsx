73225ec2c30e9c29b487f04c518da936fbcecb09
furxx2000 <furxx2000@gmail.com>
2023-03-24 07:16:02 +0800

    Import useContext as file source

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { FormContext, useFormSource } from './hook/useForm';
+import { useForm } from './hook/useForm';
 import { HelloWorld } from './component/HelloWorld';
 import { Form } from './component/Form';
 import { apiCall } from './ApiCall';
 import './css/App.css';
+
+interface LoginReq {
+  [key: string]: any;
+}
+
+interface ValidationRule {
+  [key: string]: (v: any) => boolean;
+}
+
+const validationRule = {
+  account: (v: string) => v.length > 8,
+  pwd: (v: string) => v.length > 12,
+};
+
+const defaultReq = {
+  account: '',
+  pwd: '',
+};
 
 function App() {
   function foo() {
@@ -12,11 +32,22 @@ function App() {
     return apiCall(4, 2023);
   }
 
+  function user() {
+    return apiCall(3);
+  }
+
   return (
-    <main>
-      <HelloWorld foo={foo} bar={bar} />
-      <Form />
-    </main>
+    <FormContext.Provider
+      value={useFormSource<LoginReq, ValidationRule>(
+        defaultReq,
+        validationRule
+      )}
+    >
+      <main>
+        <HelloWorld foo={foo} bar={bar} />
+        <Form />
+      </main>
+    </FormContext.Provider>
   );
 }
 
