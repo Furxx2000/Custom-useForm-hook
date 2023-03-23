d38e1a07a02ba0dc1fb13049ddc778b3f5482b65
furxx2000 <furxx2000@gmail.com>
2023-03-24 07:14:35 +0800

    Complete useForm hook with GTP's help

--- src/hook/useForm.tsx
+++ src/hook/useForm.tsx
@@ -1,16 +1,45 @@
-import { useReducer, useState, useEffect } from 'react';
+import {
+  useReducer,
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+} from 'react';
 
-export function useForm<T, U extends {}>(reqProps: T, validationRule: U) {
+// Type of validation result
+type ValidationResult<U> = {
+  [K in keyof U]: { error: boolean };
+} & { allFieldsValid: () => boolean };
+
+// Type of use reducer action
+type Action<T> = { [K in keyof T]: T[K] };
+
+export function useFormSource<
+  T extends Record<string, string>,
+  U extends Record<string, (v: any) => boolean>
+>(reqProps: T, validationRule: U) {
+  // State of login request
   const [loginReq, dispatch] = useReducer(
-    (state: T, action: any) => ({
+    (state: T, action: Action<T>) => ({
       ...state,
       ...action,
     }),
     reqProps
   );
-  const [validationResult, setValidationResult] = useState(() =>
-    convertRuleToResult(validationRule)
+
+  // State of
+  const [isReqSubmitted, setIsReqSubmitted] = useState<boolean>();
+
+  // State of validation result
+  const [validationResult, setValidationResult] = useState<ValidationResult<U>>(
+    () => convertRuleToResult(validationRule)
   );
+
+  // Check validation result when login request value change
+  useEffect(() => {
+    if (!isReqSubmitted) setIsReqSubmitted(true);
+    setValidationResult(convertRuleToResult(validationRule));
+  }, [loginReq]);
 
   function convertRuleToResult(obj: U) {
     const keyArr = Object.keys(obj);
@@ -18,37 +47,42 @@ export function useForm<T, U extends {}>(reqProps: T, validationRule: U) {
       return {
         ...accumulator,
         [value]: { error: !validationRule[value](loginReq[value]) },
-        allFieldsValid: () =>
-          keyArr.every((key) => validationRule[key](loginReq[key])),
       };
-    }, {});
-    console.log(keyObj);
-    return keyObj;
+    }, {} as { [K in keyof U]: { error: boolean } });
+
+    return {
+      ...keyObj,
+      allFieldsValid: () =>
+        keyArr.every((key) => validationRule[key](loginReq[key])),
+    };
   }
 
-  useEffect(() => {
-    const keyArr = Object.keys(validationResult);
-    setValidationResult(convertRuleToResult(validationRule));
-    // const newObj = keyArr.reduce((accumulator, value) => {
-    //   return {
-    //     ...accumulator,
-    //     [value]: { error: !validationRule[value](loginReq[value]) },
-    //   };
-    // }, {});
-    // setValidationResult(newObj);
-  }, [loginReq]);
-
   function handleSetAccount(e: React.ChangeEvent<HTMLInputElement>) {
-    dispatch({ account: e.target.value });
+    dispatch({ account: e.target.value } as unknown as Action<T>);
   }
 
   function handleSetPwd(e: React.ChangeEvent<HTMLInputElement>) {
-    dispatch({ pwd: e.target.value });
+    dispatch({ pwd: e.target.value } as unknown as Action<T>);
+  }
+
+  function handleIsReqSubmitted(value: boolean) {
+    setIsReqSubmitted(value);
   }
 
   return {
     loginReq,
     formSetter: { account: handleSetAccount, pwd: handleSetPwd },
     validationResult,
+    isReqSubmitted,
+    handleIsReqSubmitted,
   };
+}
+
+// UseContext
+export const FormContext = createContext<ReturnType<typeof useFormSource>>(
+  null as unknown as ReturnType<typeof useFormSource>
+);
+
+export function useForm() {
+  return useContext(FormContext);
 }
