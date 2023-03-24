8caff598e1b8d727cd0093aaf80e8ff5cb9715f2
furxx2000 <furxx2000@gmail.com>
2023-03-24 23:44:59 +0800

    Import isReqSubmitted and validationResult to decide to show LoadingWrapper component or not

--- src/component/HelloWorld.tsx
+++ src/component/HelloWorld.tsx
@@ -1,11 +1,15 @@
 import { LoadingWrapper } from './LoadingWrapper';
+import { useForm } from '../hook/useForm';
 
 interface Apis {
   foo(): Promise<string>;
   bar(): Promise<number>;
+  user(): Promise<string>;
 }
 
-export function HelloWorld({ foo, bar }: Apis) {
+export function HelloWorld({ foo, bar, user }: Apis) {
+  const { isReqSubmitted, validationResult } = useForm();
+
   return (
     <div className='hello-world'>
       <LoadingWrapper
@@ -13,10 +17,12 @@ export function HelloWorld({ foo, bar }: Apis) {
         renderData={(data: string) => <div className='respond'>{data}</div>}
       />
 
-      {/* <LoadingWrapper
-        loadData={() => foo()}
-        renderData={(data: string) => <div className='respond'>{data}</div>}
-      /> */}
+      {isReqSubmitted && validationResult.allFieldsValid() && (
+        <LoadingWrapper
+          loadData={() => user()}
+          renderData={(data: string) => <div className='respond'>{data}</div>}
+        />
+      )}
 
       <LoadingWrapper
         loadData={() => bar()}
